29c209afa4ccfa727f0e82f727ff007d9fbba58a
ZioPao <picchetti1998@gmail.com>
2024-05-10 21:24:06 +0200

    Fix to occupations bonuses, colors

--- media/lua/shared/PDS_Addon_DNZ/Data.lua
+++ media/lua/shared/PDS_Addon_DNZ/Data.lua
@@ -18,7 +18,8 @@ PLAYER_DICE_VALUES.OCCUPATIONS_BONUS = {
     Paesant = { Strength = 1 },
     FormerSlave = { Strength = 2, Willpower = 1 },
     Survivalist = { Wisdom = 2, Reflex = 1 },
-    Artificer = { Intelligence = 3 },
+    Artificer = { Intelligence = 2, Wisdom = 1 },
+    Scribe = { Intelligence = 3 },
     TravellingMerchant = { Charisma = 2, Willpower = 1 },
     Cowboy = { Reflex = 2, Wisdom = 1 },
     Tribesman = { Strength = 1, Reflex = 1, Wisdom = 1 },
@@ -61,36 +62,34 @@ PLAYER_DICE_VALUES.STATUS_EFFECTS = {
 }
 
 
-
--- TODO Set correct colors
 COLORS_DICE_TABLES = {
     -- Normal colors for status effects
     STATUS_EFFECTS     = {
-        Blinded = { r = 0, g = 0.68, b = 0.94 },
-        Deafened = { r = 0.95, g = 0.35, b = 0.16 },
-        Frightened = { r = 0.66, g = 0.15, b = 0.18 },
-        Grappled = { r = 1, g = 1, b = 1 },             -- FFFFFF
-        Incapacitated = { r = 1, g = 1, b = 1 },        -- FFFFFF
-        Poisoned = { r = 0.04, g = 0.58, b = 0.27 },    -- #669445
-        Prone = { r = 0.57, g = 0.15, b = 0.56 },       -- #91268f
-        Stunned = { r = 0.369, g = 0.369, b = 0.863 },  -- 94,94,220
-        Invisible = { r = 0.369, g = 0.369, b = 0.863 }, -- 94,94,220
-        Bleeding = { r = 0.369, g = 0.369, b = 0.863 }, -- 94,94,220
-        Sneaking = { r = 0.369, g = 0.369, b = 0.863 }, -- 94,94,220
+        Blinded = { r = 0.627, g = 0.043, b = 0.796 },
+        Deafened = { r = 0.478, g = 0.737, b = 0.627 },
+        Frightened = { r = 0.843, g = 0.376, b = 0.627 },
+        Grappled = { r = 0.843, g = 0.627, b = 0.376 },
+        Incapacitated = { r = 1, g = 1, b = 1 },
+        Poisoned = { r = 0.498, g = 0.804, b = 0.376 },
+        Prone = { r = 0.376, g = 0.627, b = 1 },
+        Stunned = { r = 0.369, g = 0.369, b = 0.863 },
+        Invisible = { r = 1, g = 0.627, b = 0.376 },
+        Bleeding = { r = 1, g = 0.376, b = 0.376 },
+        Sneaking = { r = 0.478, g = 0.627, b = 0.763 },
     },
 
     -- Used for color blind users
     STATUS_EFFECTS_ALT = {
-        Blinded = { r = 0, g = 0.68, b = 0.94 },
-        Deafened = { r = 0.95, g = 0.35, b = 0.16 },
-        Frightened = { r = 0.66, g = 0.15, b = 0.18 },
-        Grappled = { r = 1, g = 1, b = 1 },             -- FFFFFF
-        Incapacitated = { r = 1, g = 1, b = 1 },        -- FFFFFF
-        Poisoned = { r = 0.04, g = 0.58, b = 0.27 },    -- #669445
-        Prone = { r = 0.57, g = 0.15, b = 0.56 },       -- #91268f
-        Stunned = { r = 0.369, g = 0.369, b = 0.863 },  -- 94,94,220
-        Invisible = { r = 0.369, g = 0.369, b = 0.863 }, -- 94,94,220
-        Bleeding = { r = 0.369, g = 0.369, b = 0.863 }, -- 94,94,220
-        Sneaking = { r = 0.369, g = 0.369, b = 0.863 }, -- 94,94,220
+        Blinded = { r = 0.5, g = 0.2, b = 0.7 },
+        Deafened = { r = 0.4, g = 0.7, b = 0.6 },
+        Frightened = { r = 0.7, g = 0.3, b = 0.6 },
+        Grappled = { r = 0.7, g = 0.5, b = 0.3 },
+        Incapacitated = { r = 1, g = 1, b = 1 },
+        Poisoned = { r = 0.4, g = 0.6, b = 1 },
+        Prone = { r = 0.57, g = 0.15, b = 0.56 },
+        Stunned = { r = 0.369, g = 0.369, b = 0.863 },
+        Invisible = { r = 1, g = 0.7, b = 0.4 },
+        Bleeding = { r = 1, g = 0.4, b = 0.4 },
+        Sneaking = { r = 0.4, g = 0.6, b = 0.7 },
     }
 }
