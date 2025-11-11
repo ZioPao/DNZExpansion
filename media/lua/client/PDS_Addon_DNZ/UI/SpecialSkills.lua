e3e9b32b77e094bfe8a99bb3c183de45750ce6b1
ZioPao <picchetti1998@gmail.com>
2025-11-11 02:39:06 +0100

    Fixed label\editbox positioning

--- media/lua/client/PDS_Addon_DNZ/UI/SpecialSkills.lua
+++ media/lua/client/PDS_Addon_DNZ/UI/SpecialSkills.lua
@@ -1,13 +1,16 @@
 local SpecialSkillUI  = {}
 
+local PANEL_HEIGHT = 25
+
 ---@param container ISPanel
 ---@param specialId number
 ---@param x number
 ---@param frameHeight number
+---@param visible boolean
 function SpecialSkillUI.AddEditableSkillPanelLabel(parent, container, specialSkillText, specialId, x, frameHeight, visible)
 
     -- todo ideally we would get the name of the special skill from global mod data
-    local box = ISTextEntryBox:new(specialSkillText, x, frameHeight / 4, container.width/3, 25)
+    local box = ISTextEntryBox:new(specialSkillText, x, frameHeight / 2 - PANEL_HEIGHT/2, container.width/3, PANEL_HEIGHT)
     parent["editSpecial"..specialId] = box        -- Reference for later
     box:initialise()
     box:instantiate()
@@ -21,14 +24,16 @@ end
 ---@param specialId number
 ---@param x number
 ---@param frameHeight number
-function SpecialSkillUI.AddSpecialSkillPanelLabel(parent, container, specialSkillText, specialId, x, frameHeight)
+---@param visible boolean
+function SpecialSkillUI.AddSpecialSkillPanelLabel(parent, container, specialSkillText, specialId, x, frameHeight, visible)
 
-    local label = ISLabel:new(x, frameHeight / 4, 25, specialSkillText, 1, 1, 1, 1, UIFont.Small, true)
+    local label = ISLabel:new(x, frameHeight / 2 - PANEL_HEIGHT/2, PANEL_HEIGHT, specialSkillText, 1, 1, 1, 1, UIFont.Small, true)
 
     -- todo reference should be based on id of special sub skill, not name
     parent["labelSpecial"..specialId] = label        -- Reference for later
     label:initialise()
     label:instantiate()
+    label:setVisible(visible)
     container:addChild(label)
 end
 
